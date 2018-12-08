1e4c031a03fd932a609b08b7c5a28abfe6031951
George J. Danforth <georgejdanforth@gmail.com>
2018-12-08 16:45:58 -0500

    basic working lambda

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject origin-proxy "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot origin-proxy.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [uswitch/lambada "0.1.2"]]
+  :profiles {:uberjar {:aot :all}}
+  :uberjar-name "origin-proxy.jar")
--- src/origin_proxy/core.clj
+++ src/origin_proxy/core.clj
@@ -1,7 +1,10 @@
 (ns origin-proxy.core
-  (:gen-class))
+  (:require [uswitch.lambada.core :refer [deflambdafn]]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(deflambdafn origin-proxy.core.OriginProxy
+  [in out context]
+  (let [event (json/read (io/reader in))]
+    (with-open [w (io/writer out)]
+      (json/write event w))))
