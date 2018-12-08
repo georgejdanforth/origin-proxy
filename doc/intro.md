fd2c852def06a998fdabde7596b20e6a9a23c8bf
George J. Danforth <georgejdanforth@gmail.com>
2018-12-08 16:04:08 -0500

    initializing proj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to origin-proxy
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/origin_proxy/core_test.clj
@@ -0,0 +1,7 @@
+(ns origin-proxy.core-test
+  (:require [clojure.test :refer :all]
+            [origin-proxy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
