cc69279dbc81359c41c71d770110135c20a40896
George J. Danforth <georgejdanforth@gmail.com>
2018-12-12 22:35:48 -0500

    fixing a whole bunch of response handling things

--- src/origin_proxy/core.clj
+++ src/origin_proxy/core.clj
@@ -17,18 +17,31 @@
   (let [headers (get event "headers") body (get event "body")]
     {:throw-exceptions false
      :headers headers
-     :body(if (= (get headers "Content-Type") "application/json")
+     :body (if (= (get headers "Content-Type") "application/json")
              (json/write-str body)
              (str body))}))
 
 (defn error-response [status error]
-  {:statusCode status :error error})
+  {:statusCode status
+   :body {:error error}
+   :headers {"content-type" "application/json"}
+   :isBase64Encoded false})
+
+(defn filter-headers [headers]
+  (apply hash-map (flatten (filter (fn [[k v]] (not (coll? v))) headers))))
+
+(defn handle-response [response]
+  (assoc
+    (rename-keys
+      (select-keys response [:body :status])
+      {:status :statusCode})
+    :isBase64Encoded false :headers (filter-headers (response :headers))))
 
 (defn make-request [url event]
   (let [request-config (get-request-config event)]
     (case (get event "httpMethod")
-      "GET" (http/get url request-config)
-      "POST" (http/post url request-config)
+      "GET" (handle-response (http/get url request-config))
+      "POST" (handle-response (http/post url request-config))
       (error-response 405 "Method not allowed."))))
 
 (defn is-valid-host [url]
@@ -42,8 +55,8 @@
 (defn handle [event]
   (let [url (get-url event)]
     (if (is-valid-host url)
-      (select-keys (make-request url event) [:headers :body])))
-      (error-response 403 "Invalid host."))
+      (make-request url event)
+      (error-response 403 "Invalid host."))))
 
 (deflambdafn origin-proxy.core.OriginProxy
   [in out context]
